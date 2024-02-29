f550f72b091ed4c512f2f2c4da38df1cb7bed9d2
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2024-02-29 22:54:26 +0200

    Add More Exercises

--- More Exercises/Conditional Statements Advanced/02.BikeRace/app.go
+++ More Exercises/Conditional Statements Advanced/02.BikeRace/app.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var route string
+	var tax float32
+	var juniors, seniors, cyclists int
+
+	fmt.Scan(&juniors, &seniors, &route)
+
+	cyclists = juniors + seniors
+
+	if route == "trail" {
+		tax = float32(juniors)*5.50 + float32(seniors)*7
+
+	} else if route == "cross-country" {
+		tax = float32(juniors)*8.0 + float32(seniors)*9.50
+
+		if cyclists >= 50 {
+			tax *= 0.75
+		}
+
+	} else if route == "downhill" {
+		tax = float32(juniors)*12.25 + float32(seniors)*13.75
+
+	} else if route == "road" {
+		tax = float32(juniors)*20 + float32(seniors)*21.50
+
+	}
+
+	tax *= 0.95
+
+	fmt.Printf("%.2f", tax)
 }
+

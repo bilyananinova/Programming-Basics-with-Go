600803ebe41067e99682f821e1a9e505454979c3
bilyananinova <57565885+bilyananinova@users.noreply.github.com>
2024-03-17 18:13:49 +0200

    Add Exam Preparation

--- Exams Prep/Programming Basics Online Exam - 6 and 7 July 2019/Problem04/app.go
+++ Exams Prep/Programming Basics Online Exam - 6 and 7 July 2019/Problem04/app.go
@@ -8,29 +8,25 @@ import (
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	var profit, money float32
+	var target, income float32
 	var coctail string
 	var order int
-	fmt.Scanln(&profit)
-	fmt.Scanln(&coctail)
+	fmt.Scanln(&target)
+	scanner.Scan()
+	coctail = scanner.Text()
 
 	for coctail != "Party!" {
 		fmt.Scanln(&order)
 
-		price := float32(len(coctail) * order)
+		var price float32 = float32(len(coctail) * order)
 
 		if int(price)%2 == 1 {
-			price *= 0.75
+			price = float32(price * 0.75)
 		}
 
-		money += price
+		income += float32(price)
 
-		if money >= profit {
-			fmt.Println("Target acquired.")
-			break
-		}
-
-		if coctail == "Party!" {
+		if income >= target {
 			break
 		}
 
@@ -39,9 +35,12 @@ func main() {
 	}
 
 	if coctail == "Party!" {
-		fmt.Printf("We need %.2f leva more.\n", profit-money)
+		fmt.Printf("We need %.2f leva more.\n", target-income)
+	} else {
+		fmt.Println("Target acquired.")
 	}
 
-	fmt.Printf("Club income - %.2f leva.", money)
+	fmt.Printf("Club income - %.2f leva.", income)
 
 }
+
